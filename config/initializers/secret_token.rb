58084668a0d9ba8d9ed43a0b49fdf6cc127239e8
v0ffka <vladimir.nudelman@gmail.com>
2015-02-21 23:51:46 +0400

    ok, let's do this as per README and fix assets issue

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || '839318b4270149f387399202ee51c49ddf0b21fb06f1e3b1ada14a8dec0fdcc54293dc9555b9604b192b01ec50de3d11ccf68966fb824477186c91cf6e290b4e'
