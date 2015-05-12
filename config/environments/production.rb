726f48784f33dd85c38c8cafc4473e66209a6a22
v0ffka <vladimir.nudelman@gmail.com>
2015-05-12 18:08:27 +0400

    change deployment url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'awanga.fr' }
+  config.action_mailer.default_url_options = { :host => 'lancement.awanga.fr' }
   config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 587,
