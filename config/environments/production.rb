26be11c714bde6f8ee73fb4075d2ad43d9b4ecde
v0ffka <vladimir.nudelman@gmail.com>
2015-05-12 16:54:22 +0400

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,16 @@ Prelaunchr::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.example.com' }
+  config.action_mailer.default_url_options = { :host => 'awanga.fr' }
+  config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 587,
+      :enable_starttls_auto => true,
+      :user_name => "adrien.belhomme@ieseg.fr",
+      :password  => "S4zRzL0WVFZLFVQLuExprA",
+      :authentication => 'login',
+      :domain => 'awanga.fr'
+  }
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
